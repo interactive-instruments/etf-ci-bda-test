3773b2865478fc0df7b939890d610089c19344b1
Jon Herrmann <herrmann@interactive-instruments.de>
2018-09-17 09:24:05 +0200

    Tests added

---
+++ src/test/java/NoTest.java
@@ -0,0 +1,13 @@
+// Just a simple JAVA test class for testing bda and ci
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class NoTest
+{
+
+  @Test
+  public void testNothing() {
+    assertEquals("one", "one");
+  }
+}
